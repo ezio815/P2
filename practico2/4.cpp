bab19e83b30c1f9f3a20204ca30194565e717c26
Ezi <exio78@hotmail.com>
2022-03-18 14:35:27 -0300

    p2/ej6

--- practico2/4.cpp
+++ practico2/4.cpp
@@ -1,13 +1,13 @@
 #include "general.h"
 #include <stdio.h>
 
-int maxComDivRec(uint n, uint a, uint b) {
+uint maxComDivRec(uint n, uint a, uint b) {
     if (a % n == 0 && b % n == 0)
         return n;
     return maxComDivRec(n - 1, a, b);
 }
 
-int maxComDiv(uint a, uint b) {
+uint maxComDiv(uint a, uint b) {
     uint min;
     if (a <= b)
         min = a;
---
+++ practico2/6.cpp
@@ -0,0 +1,16 @@
+#include "general.h"
+#include <stdio.h>
+
+uint factAcum ( uint n , int acum ) {
+    if (n == 0)
+        return acum;
+    return factAcum(n - 1, n * acum);
+}
+
+int main() {
+    uint n;
+    printf("Ingrese un número entero no negativo: ");
+    scanf("%d", &n);
+    printf("El factorial de %d es %d\n", n, factAcum(n, 1));
+    return 0;
+}
